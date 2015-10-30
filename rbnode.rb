e060aaf89448d14bd14541a89e4905ae5d4bdbfb
emilycw <emily.rose.cw@gmail.com>
2015-10-30 14:37:04 +1100

    write include? function for node and tree

--- rbnode.rb
+++ rbnode.rb
@@ -6,4 +6,11 @@ class RedBlackNode
     @right = right
     @black = black
   end
+
+  def include?(value)
+    return false if @value.nil?
+    return true if @value == value
+    return @left.include?(value) if @value > value && @left
+    @right && @right.include?(value)
+  end
 end
--- rbtree.rb
+++ rbtree.rb
@@ -1,5 +1,15 @@
 class RedBlackTree
   def initialize()
-    @rootNode = RedBlackNode.new(nil)
+    @rootNode = RedBlackNode.new(nil, nil, nil, nil, true)
+  end
+
+  def add(value)
+  end
+
+  def remove(value)
+  end
+
+  def include?(value)
+    @rootNode.include?(value)
   end
 end
